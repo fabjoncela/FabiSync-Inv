0716764d3fad22d2ee61483c7ca76dcc290b9327
fabjoncela <fabjoncela@gmail.com>
2024-09-05 21:12:29 +0200

    CRUD works, local place safe

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -91,9 +91,7 @@ const Products = () => {
           <div key={product.productId} className="border shadow rounded-md p-4 max-w-full w-full mx-auto">
             <div className="flex flex-col items-center">
               <Image
-                src={`https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/product${
-                  Math.floor(Math.random() * 3) + 1
-                }.png`}
+                src={``}
                 alt={product.name}
                 width={150}
                 height={150}
